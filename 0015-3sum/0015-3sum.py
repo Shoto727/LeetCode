96a63dd3b45b83ed21b0fe655d9a4f6d0553c2b0
Shoto727 <87116024+Shoto727@users.noreply.github.com>
2026-03-24 22:08:36 +0530

    Time: 639 ms (54.62%), Space: 22.3 MB (44.06%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,31 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        n = len(nums)
-        answer = []
+        res = []
 
-        for i in range(n):
-            if nums[i] > 0:
-                break
-            elif i > 0 and nums[i] == nums[i - 1]:
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            lo, hi = i + 1, n - 1
-            while lo < hi:
-                summ = nums[i] + nums[lo] + nums[hi]
+            left, right = i + 1, len(nums) - 1
 
-                if summ == 0:
-                    answer.append([nums[i], nums[lo], nums[hi]])
-                    lo += 1
-                    hi -= 1
-                    while lo < hi and nums[lo] == nums[lo - 1]:
-                        lo += 1
-                    while lo < hi and nums[hi] == nums[hi + 1]:
-                        hi -= 1
-                elif summ < 0:
-                    lo += 1
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                if curr_sum == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    # skip duplicates
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif curr_sum < 0:
+                    left += 1
                 else:
-                    hi -= 1
+                    right -= 1
 
-        return answer
+        return res
